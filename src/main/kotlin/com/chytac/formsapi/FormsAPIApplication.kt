ed2f1cbdb2a4c0899957bf6e2323f70be5b3e720
KubaX24 <2451@post.cz>
2022-06-06 22:25:13 +0200

    Project created v1

--- src/main/kotlin/com/chytac/formsapi/FormsAPIApplication.kt
+++ src/main/kotlin/com/chytac/formsapi/FormsAPIApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ProjectdoodleApplication
+class FormsAPIApplication
 
 fun main(args: Array<String>) {
-    runApplication<ProjectdoodleApplication>(*args)
+    runApplication<FormsAPIApplication>(*args)
 }
--- src/main/kotlin/com/chytac/formsapi/entities/EventEntity.kt
+++ src/main/kotlin/com/chytac/formsapi/entities/EventEntity.kt
@@ -1,12 +1,31 @@
 package com.chytac.formsapi.entities
 
-import org.springframework.data.annotation.Id
+import org.hibernate.Hibernate
 import java.time.LocalDate
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.Id
 
+@Entity
 data class EventEntity(
     @Id @GeneratedValue val id: Int,
     val name: String,
-    val description: String,
-    val dates: List<LocalDate>
-    )
+    val description: String
+    //TODO: add array of dates (new entity for dates)
+    ) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as EventEntity
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id )"
+    }
+}
